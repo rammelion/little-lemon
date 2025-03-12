e9a9775ed27e38840ec8a47c34dfbbd84c246c80
rammelion <61567322+rammelion@users.noreply.github.com>
2025-03-12 06:21:27 +0100

    2025.03.11 second

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Suspense, lazy } from 'react'
+//import { Suspense, lazy } from 'react'
 import Nav from './components/semantics/Nav'
 import Header from './components/semantics/Header'
 import Main from './components/semantics/Main'
 import Footer from './components/semantics/Footer'
-import GDPR from './components/GDPR'
+//import GDPR from './components/GDPR'
 
 
 // Works also with SSR as expected
--- src/components/GDPR.js
+++
@@ -1,8 +0,0 @@
-import Container from "./Container"
-export default function GDPR(props) {
-    return (
-        <Container idName='gdpr' className = 'container'>
-            GDPR Container
-        </Container>
-    )
-}
